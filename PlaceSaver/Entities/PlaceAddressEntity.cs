63baef11a35e6a078719cdcce10af09a35fb561f
kamil <kamil.kurzaj04@gmail.com>
2025-07-19 13:38:44 +0200

    Created Entities

---
+++ PlaceSaver/Entities/PlaceAddressEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace PlaceSaver.Entities;
+
+public class PlaceAddressEntity
+{
+    public int Id { get; set; }             
+    public string Street { get; set; }      
+    public string City { get; set; }        
+    public string State { get; set; }       
+    public string Country { get; set; }     
+    public string PostalCode { get; set; } 
+    public int PlaceEntityId { get; set; }  
+    public PlaceEntity PlaceEntity { get; set; }
+}
---
+++ PlaceSaver/Entities/PlaceEntity.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PlaceSaver.Entities;
+
+public class PlaceEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Vicinity { get; set; }
+    public string Photo { get; set; }
+    public DateTime? EstimatedStartDate { get; set; }
+    public DateTime? EstimatedEndDate { get; set; }
+    public int PlaceAddressId { get; set; }
+    public PlaceAddressEntity PlaceAddress { get; set; } 
+    public string GoogleId { get; set; }
+}
